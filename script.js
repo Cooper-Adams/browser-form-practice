cdac84c226fa3ecfcfa9c95fcca2856c01f1ce62
Cooper Adams <cooperadamsbusiness@gmail.com>
2022-02-17 00:27:14 -0500

    Initial JS commit with form inputs selected

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipCode = document.getElementById("zip");
+const password = document.getElementById("password");
+
+//Use onfocus() and onblur() for appropriate highlights and validations
